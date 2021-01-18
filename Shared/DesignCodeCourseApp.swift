7e9efa0ecc3d358ecc3e9790eb1e707f53cb8739
张亚飞 <672233580@qq.com>
2021-01-18 09:58:00 +0800

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by 亚飞 on 2021/1/18.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
